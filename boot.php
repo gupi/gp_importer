5e58f301bb13338310d76e4841c72481a3f7b870
Gunter Pietzsch <gunter.pietzsch@prestosoft.de>
2016-02-22 13:48:24 +0100

    Added files via upload

---
+++ boot.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var rex_addon $this */
+
+// Diese Datei ist keine Pflichdatei mehr.
+
+// Daten wie Autor, Version, Subpages etc. sollten wenn möglich in der package.yml notiert werden.
+// Sie können aber auch weiterhin hier gesetzt werden:
+$this->setProperty('author', 'Gregor Harlan');
+
+// Die Datei sollte keine veränderbare Konfigurationen mehr enthalten, um die Updatefähigkeit zu erhalten.
+// Stattdessen sollte dafür die rex_config verwendet werden (siehe install.php)
+
+// Klassen und lang-Dateien müssen hier nicht mehr eingebunden werden, sie werden nun automatisch gefunden.
+
+// Addonrechte (permissions) registieren
+if (rex::isBackend() && is_object(rex::getUser())) {
+    rex_perm::register('gpimporter[]');
+    rex_perm::register('gpimporter[config]');
+}
